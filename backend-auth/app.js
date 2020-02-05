aec7663d3ad5ba175d90f76be6dd7ba163190481
Alejo <alejandrofranco@pursuit.org>
2020-02-04 20:41:01 -0500

    set up server to serve build react app

--- backend-auth/app.js
+++ backend-auth/app.js
@@ -7,7 +7,6 @@ var session = require('express-session')
 var passport = require('./auth/passport')
 var cors = require('cors')
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var authRouter = require('./routes/auth');
 
@@ -18,7 +17,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../frontend-auth/build')));
+
 app.use(session({
   secret: "NOT_A_GOOD_SECRET",
   resave: false,
@@ -29,8 +30,14 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 // Routes
-app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
+
+console.log('__dirname', __dirname)
+
+app.use('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend-auth/build/index.html"))
+})
 
 module.exports = app;
